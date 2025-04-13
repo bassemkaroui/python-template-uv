484e5a274199ad08d91340e18786251ff01074f2
Bassem Karoui <bassem.karoui1@gmail.com>
2025-04-13 13:32:46 +0200

    ✨ feat(copier): initialize copier config and template

---
+++ extensions.py
@@ -0,0 +1,25 @@
+import subprocess  # noqa: S404
+from datetime import date
+
+from jinja2.ext import Extension
+
+
+def git_user_name(default: str) -> str:
+    return subprocess.getoutput("git config user.name").strip() or default  # noqa: S605,S607
+
+
+def git_user_email(default: str) -> str:
+    return subprocess.getoutput("git config user.email").strip() or default  # noqa: S605,S607
+
+
+class GitExtension(Extension):
+    def __init__(self, environment):
+        super().__init__(environment)
+        environment.filters["git_user_name"] = git_user_name
+        environment.filters["git_user_email"] = git_user_email
+
+
+class CurrentYearExtension(Extension):
+    def __init__(self, environment):
+        super().__init__(environment)
+        environment.globals["current_year"] = date.today().year
