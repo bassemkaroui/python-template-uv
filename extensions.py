8fb763c6ade01819e361062bf89d599622eb6631
Bassem Karoui <bassem.karoui1@gmail.com>
2025-04-15 10:49:40 +0200

    ✨ feat: scaffold initial beta version of the Python template

--- extensions.py
+++ extensions.py
@@ -12,11 +12,21 @@ def git_user_email(default: str) -> str:
     return subprocess.getoutput("git config user.email").strip() or default  # noqa: S605,S607
 
 
+def are_tags_gpg_signed(default: bool = False) -> bool:
+    value = subprocess.getoutput("git config --bool tag.gpgsign").strip().lower()  # noqa: S605,S607
+    if value in ["true", "1", "yes", "on"]:
+        return True
+    elif value in ["false", "0", "no", "off"]:
+        return False
+    return default
+
+
 class GitExtension(Extension):
     def __init__(self, environment):
         super().__init__(environment)
         environment.filters["git_user_name"] = git_user_name
         environment.filters["git_user_email"] = git_user_email
+        environment.filters["are_tags_gpg_signed"] = are_tags_gpg_signed
 
 
 class CurrentYearExtension(Extension):
